42e58e50e5b5fe54d5692a97001cda3910950660
shibashis <shibashismallik@gmail.com>
2019-05-29 20:23:17 +0530

    made django app

---
+++ TVD/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TvdConfig(AppConfig):
+    name = 'TVD'
---
+++ TVD/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'detection'
+
+urlpatterns = [
+    url(r'/', views.homepage, name='homepage'),
+    url(r'requirements/$', views.requirements, name='homepage')
+]
