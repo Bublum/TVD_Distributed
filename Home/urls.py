c77a010c40470f050006c1f7f66d443755bfff99
akzarma <akzarma@gmail.com>
2019-05-29 23:57:00 +0530

    some edits

--- Home/urls.py
+++ Home/urls.py
@@ -6,5 +6,6 @@ app_name = 'detection'
 urlpatterns = [
     # url(r'/', views.homepage, name='homepage'),
     url(r'^requirements/$', views.requirements, name='requirements'),
-    url(r'^zip/$', views.zip, name='zip')
+    url(r'^zip/$', views.zip, name='zip'),
+    url(r'^get_files/$', views.get_files, name='get_files'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,16 +1,12 @@
 import os
 import zipfile
 
-import io
 from django.http import HttpResponse
-
-from django.shortcuts import render
 
 from TVD_Distributed.settings import MEDIA_ROOT, MEDIA_URL
 
 
 def requirements(request):
-
     # zip_subdir = "/home/shibashis/Videos/something"
     # zip_filename = zip_subdir + ".zip"
     #
@@ -42,6 +38,22 @@ def requirements(request):
 
     return HttpResponse(MEDIA_URL + filename)
 
+
 #
 def zip(request):
     return HttpResponse(MEDIA_URL + 'requirements.zip')
+
+
+def get_files(request):
+    if request.method == 'GET':
+        folder_path_to_send = MEDIA_ROOT + '/to_send/helmet/'
+        filename = 'get_files.zip'
+        zipf = zipfile.ZipFile(os.path.join(MEDIA_ROOT, filename), 'w', zipfile.ZIP_DEFLATED)
+        files = os.listdir(folder_path_to_send)
+
+        for file in files:
+            zipf.write(os.path.join(folder_path_to_send, file), file)
+
+        zipf.close()
+
+        return HttpResponse(MEDIA_URL + filename)
